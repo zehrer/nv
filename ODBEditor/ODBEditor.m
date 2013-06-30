12370d42ba390ee477c5f352d8548455cbfd3798
Zachary Waldowski <zwaldowski@gmail.com>
2013-06-30 19:42:48 -0400

    Replace use of -[NSWorkspace launchedApplications].

--- ODBEditor/ODBEditor.m
+++ ODBEditor/ODBEditor.m
@@ -209,19 +209,13 @@ beepReturn:
 	BOOL success = NO;
 	BOOL running = NO;
 	NSWorkspace	*workspace = [NSWorkspace sharedWorkspace];
-	NSArray	*runningApplications = [workspace launchedApplications];
-	NSEnumerator *enumerator = [runningApplications objectEnumerator];
-	NSDictionary *applicationInfo;
-	
+	NSArray *runningApplications = [workspace runningApplications];
 	NSString *editorBundleIdentifier = [ed bundleIdentifier];
-	
-	while (nil != (applicationInfo = [enumerator nextObject])) {
-		NSString *bundleIdentifier = [applicationInfo objectForKey: @"NSApplicationBundleIdentifier"];
-		
+	for (NSRunningApplication *application in runningApplications) {
+		NSString *bundleIdentifier = application.bundleIdentifier;
 		if ([bundleIdentifier isEqualToString: editorBundleIdentifier]) {
 			running = YES;
-			// bring the app forward
-			success = [workspace launchApplication: [applicationInfo objectForKey: @"NSApplicationPath"]];
+			success = [application activateWithOptions:0];
 			break;
 		}
 	}
